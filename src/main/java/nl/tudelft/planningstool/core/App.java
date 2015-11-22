87139ef2deee5a5bc0275a905692da13d0390b37
GijsWeterings <g.weterings@student.tudelft.nl>
2015-11-22 13:08:23 +0100

    Remove old authentication basic, Found a better way

--- src/main/java/nl/tudelft/planningstool/core/App.java
+++ src/main/java/nl/tudelft/planningstool/core/App.java
@@ -3,17 +3,9 @@ package nl.tudelft.planningstool.core;
 import com.google.inject.Injector;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-//import nl.tudelft.planningstool.core.saml.SAMLAuthenticator;
-//import nl.tudelft.planningstool.core.saml.SAMLLoginService;
-import org.eclipse.jetty.security.*;
-import org.eclipse.jetty.security.authentication.FormAuthenticator;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.util.security.Constraint;
-import org.eclipse.jetty.security.authentication.BasicAuthenticator;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.eclipse.jetty.server.session.HashSessionIdManager;
-import org.eclipse.jetty.util.security.Credential;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import java.util.concurrent.atomic.AtomicReference;
@@ -33,51 +25,8 @@ public class App {
         this.server.setHandler(this.attachHandlers());
     }
 
-    private SecurityHandler basicAuth() {
-
-        HashLoginService l = new HashLoginService();
-        l.putUser("sjaars", Credential.getCredential("slapwachtwoord"), new String[]{"student"});
-        l.putUser("gijs", Credential.getCredential("superduperwachtwoord"), new String[]{"student", "teacher"});
-        l.setName("planningstool");
-
-//        SAMLLoginService saml = new SAMLLoginService();
-//        saml.setName("SamlLogin");
-
-        // Create constraint to
-        Constraint constraint = new Constraint();
-        constraint.setName(Constraint.__BASIC_AUTH);
-        constraint.setRoles(new String[]{"student"});
-        constraint.setAuthenticate(true);
-
-        ConstraintMapping cm = new ConstraintMapping();
-        cm.setConstraint(constraint);
-        cm.setPathSpec("/*");
-
-
-        Constraint constraint2 = new Constraint();
-        constraint2.setName(Constraint.__BASIC_AUTH);
-        constraint2.setRoles(new String[]{"teacher"});
-        constraint2.setAuthenticate(true);
-
-        ConstraintMapping cmAdmin = new ConstraintMapping();
-        cmAdmin.setConstraint(constraint2);
-        cmAdmin.setPathSpec("/v1/users/USER-aba62cd5-caa6-4e42-a5d6-4909f03038bf/courses/*");
-
-        ConstraintSecurityHandler csh = new ConstraintSecurityHandler();
-        csh.setAuthenticator(new BasicAuthenticator());
-        //csh.setAuthenticator(new SAMLAuthenticator());
-        csh.setRealmName("planningstool");
-        csh.addConstraintMapping(cm);
-        csh.addConstraintMapping(cmAdmin);
-        csh.setLoginService(l);
-        //csh.setLoginService(saml);
-
-        return csh;
-    }
-
     private ContextHandlerCollection attachHandlers() {
         final RequestHandler requestHandler = new RequestHandler(this);
-        requestHandler.setSecurityHandler(basicAuth());
 
         final ContextHandlerCollection handlers = new ContextHandlerCollection();
         handlers.addContext("/", "/").setHandler(requestHandler);
